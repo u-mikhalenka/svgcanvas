3bf121189ae399c7b191a86b27d3a339f9e2a6f8
Vladimir Mikhalenko <mikhalenko@acdlabs.by>
2024-03-20 22:03:18 +0000

    format files

--- rollup.config.js
+++ rollup.config.js
@@ -2,34 +2,34 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default [
-    {
-        input: 'index.js',
-        output: {
-            file: 'dist/svgcanvas.js',
-            format: 'cjs'
-        }
+  {
+    input: 'index.js',
+    output: {
+      file: 'dist/svgcanvas.js',
+      format: 'cjs',
     },
-    {
-        input: 'index.js',
-        output: {
-            file: 'dist/svgcanvas.esm.js',
-            format: 'esm'
-        }
+  },
+  {
+    input: 'index.js',
+    output: {
+      file: 'dist/svgcanvas.esm.js',
+      format: 'esm',
     },
-    {
-        input: 'test/index.js',
-        output: {
-            file: 'dist/test.js',
-            format: 'iife'
-        }
+  },
+  {
+    input: 'test/index.js',
+    output: {
+      file: 'dist/test.js',
+      format: 'iife',
     },
-    {
-        input: 'test/rendering.test.js',
-        output: {
-            file: 'dist/rendering.test.js',
-            format: 'iife',
-            sourcemap: true,
-        },
-        plugins: [nodeResolve(), commonjs()]
+  },
+  {
+    input: 'test/rendering.test.js',
+    output: {
+      file: 'dist/rendering.test.js',
+      format: 'iife',
+      sourcemap: true,
     },
-]
+    plugins: [nodeResolve(), commonjs()],
+  },
+];
--- utils.js
+++ utils.js
@@ -1,19 +1,19 @@
 const DEBUG = true;
 
 function toString(obj) {
-    if (!obj) {
-        return obj
-    }
-    if (typeof obj === 'string') {
-        return obj
-    }
-    return obj + '';
+  if (!obj) {
+    return obj;
+  }
+  if (typeof obj === 'string') {
+    return obj;
+  }
+  return obj + '';
 }
 
 function debug(...data) {
-    if (DEBUG) {
-        console.debug(...data)
-    }
+  if (DEBUG) {
+    console.debug(...data);
+  }
 }
 
-export {toString, debug};
+export { toString, debug };
